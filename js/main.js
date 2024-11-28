87f2c5fcc3fded2f5578eda687ea6902f0b78f3c
catwilo <31023673+catwilo@users.noreply.github.com>
2024-11-27 21:43:45 -0500

    Cambios específicos en dev

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var offcanvas = new bootstrap.Offcanvas(document.getElementById('sidebar'));
+  offcanvas.show(); // Mostrar offcanvas automáticamente al cargar
+});
